c002d9c6389893400ccd0fec8916f54e44a0022b
Daniel Guillén Carrasco <danielics51@gmail.com>
2026-03-17 09:43:15 +0100

    Actualització

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function changeContent(page) {
   switch (page) {
     case 'home':
       contentDiv.innerHTML = `
-        <img src="heavy.png" alt="Heavy Soul">
+        <img src="logo.png" alt="Heavy Soul logo" id="main-logo">
         <h2>Welcome to Heavy Soul!</h2>
         <p>
           This is the home page of the heaviest music hub on the web.
@@ -15,23 +15,23 @@ function changeContent(page) {
       break;
     case 'news':
       contentDiv.innerHTML = `
-        <h2>Latest News</h2>
+        <h2>Últimes notícies</h2>
         <ul>
-          <li>New album drop from The Dark Riders</li>
-          <li>Summer Steel Festival announces 2026 lineup</li>
-          <li>Exclusive interview with vocalist Raven Black</li>
+          <li>Nou àlbum de Metallica</li>
+          <li>Summer Steel Festival anuncia les bandes de 2026</li>
+          <li>ENtrevista exclusiva amb el guitarrista de Iron Maiden</li>
         </ul>
       `;
       break;
-    case 'followers':
+    case 'Followers':
       contentDiv.innerHTML = `
         <h2>From Our Followers</h2>
         <p>
-          We love hearing from you! Here are some messages and photos sent in by our fans.
+         Ens encanta rebre missatges dels nostres seguidors! Aquí teniu algunes de les últimes paraules d'ànim que hem rebut:
         </p>
         <ul>
-          <li>"Thanks for keeping the scene alive!" – Max</li>
-          <li>"Your site is pure fire." – Elena</li>
+          <li>"Gràcies per mantenir el metal viu!" – Max</li>
+          <li>"Me encanta aquesta web." – Elena</li>
           <li>"Rock on forever!" – The Steel Clan</li>
         </ul>
       `;
